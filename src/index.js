ba0612c870e43262de0fa95eaf329601cb9adc71
ofersarid <ofersarid@gmail.com>
2020-03-23 13:28:50 +0200

    added support for redux dev tools

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import { createStore } from 'redux';
 import Root from './root';
 import combineReducers from './combine-reducers';
 
-const store = createStore(combineReducers);
+const store = createStore(combineReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 render(<Root store={store} />, document.getElementById('root'));
